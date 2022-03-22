01bb8b8a1f92d6072b1dd7200eefc54a31f729c3
Prasanna Tandukar <prasannatandukar@pop-os.localdomain>
2022-03-22 15:01:42 +0545

    Added carousel feature and fixed a bug on centering nav elements vertically

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+// function for slider carousel
+const slider = () => {
+  const slides = document.querySelectorAll(".slide");
+  const nextBtn = document.querySelector(".nextBtnSlider");
+  const prevBtn = document.querySelector(".prevBtnSlider");
+
+  slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
+  });
+
+  let counter = 0;
+  nextBtn.addEventListener("click", () => {
+    counter++;
+    carousel();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    counter--;
+    carousel();
+  });
+
+  const carousel = () => {
+    if (counter < 0) {
+      counter = 0;
+    }
+    if (counter < slides.length - 1) {
+      nextBtn.style.display = "block";
+    } else {
+      nextBtn.style.display = "none";
+    }
+    if (counter > 0) {
+      prevBtn.style.display = "block";
+    } else {
+      prevBtn.style.display = "none";
+    }
+
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+  };
+
+  prevBtn.style.display = "none";
+};
+
+// Function for hamburger menu of navigation bar
 function toggleMenu() {
   let button = document.querySelector(".menu-icon");
   button.addEventListener("click", (e) => {
@@ -6,6 +51,8 @@ function toggleMenu() {
   });
 }
 
+// This function runs when all of the DOM's content (ie. HTML, CSS) loads
 window.addEventListener("DOMContentLoaded", (e) => {
   toggleMenu();
+  slider();
 });
